de077518963a9597fa977fca58c7c7c5d308ffba
Alexandre Dossin <alexandre.dossin@outlook.fr>
2025-07-05 15:14:47 +0200

    Initialize subgraph

--- arbitrum/mondapp-arbitrum/src/monde-app-yield-manager-arbitrum.ts
+++ arbitrum/mondapp-arbitrum/src/monde-app-yield-manager-arbitrum.ts
@@ -1,17 +1,8 @@
 import {
   DepositProcessed as DepositProcessedEvent,
-  RoleAdminChanged as RoleAdminChangedEvent,
-  RoleGranted as RoleGrantedEvent,
-  RoleRevoked as RoleRevokedEvent,
   WithdrawProcessed as WithdrawProcessedEvent
 } from "../generated/MondeAppYieldManagerArbitrum/MondeAppYieldManagerArbitrum"
-import {
-  DepositProcessed,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  WithdrawProcessed
-} from "../generated/schema"
+import { DepositProcessed, WithdrawProcessed } from "../generated/schema"
 
 export function handleDepositProcessed(event: DepositProcessedEvent): void {
   let entity = new DepositProcessed(
@@ -22,51 +13,6 @@ export function handleDepositProcessed(event: DepositProcessedEvent): void {
   entity.user = event.params.user
   entity.amount = event.params.amount
   entity.shares = event.params.shares
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
-  let entity = new RoleAdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleGranted(event: RoleGrantedEvent): void {
-  let entity = new RoleGranted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleRevoked(event: RoleRevokedEvent): void {
-  let entity = new RoleRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
--- arbitrum/mondapp-arbitrum/tests/monde-app-yield-manager-arbitrum-utils.ts
+++ arbitrum/mondapp-arbitrum/tests/monde-app-yield-manager-arbitrum-utils.ts
@@ -2,9 +2,6 @@ import { newMockEvent } from "matchstick-as"
 import { ethereum, Bytes, Address, BigInt } from "@graphprotocol/graph-ts"
 import {
   DepositProcessed,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
   WithdrawProcessed
 } from "../generated/MondeAppYieldManagerArbitrum/MondeAppYieldManagerArbitrum"
 
@@ -42,78 +39,6 @@ export function createDepositProcessedEvent(
   )
 
   return depositProcessedEvent
-}
-
-export function createRoleAdminChangedEvent(
-  role: Bytes,
-  previousAdminRole: Bytes,
-  newAdminRole: Bytes
-): RoleAdminChanged {
-  let roleAdminChangedEvent = changetype<RoleAdminChanged>(newMockEvent())
-
-  roleAdminChangedEvent.parameters = new Array()
-
-  roleAdminChangedEvent.parameters.push(
-    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
-  )
-  roleAdminChangedEvent.parameters.push(
-    new ethereum.EventParam(
-      "previousAdminRole",
-      ethereum.Value.fromFixedBytes(previousAdminRole)
-    )
-  )
-  roleAdminChangedEvent.parameters.push(
-    new ethereum.EventParam(
-      "newAdminRole",
-      ethereum.Value.fromFixedBytes(newAdminRole)
-    )
-  )
-
-  return roleAdminChangedEvent
-}
-
-export function createRoleGrantedEvent(
-  role: Bytes,
-  account: Address,
-  sender: Address
-): RoleGranted {
-  let roleGrantedEvent = changetype<RoleGranted>(newMockEvent())
-
-  roleGrantedEvent.parameters = new Array()
-
-  roleGrantedEvent.parameters.push(
-    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
-  )
-  roleGrantedEvent.parameters.push(
-    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
-  )
-  roleGrantedEvent.parameters.push(
-    new ethereum.EventParam("sender", ethereum.Value.fromAddress(sender))
-  )
-
-  return roleGrantedEvent
-}
-
-export function createRoleRevokedEvent(
-  role: Bytes,
-  account: Address,
-  sender: Address
-): RoleRevoked {
-  let roleRevokedEvent = changetype<RoleRevoked>(newMockEvent())
-
-  roleRevokedEvent.parameters = new Array()
-
-  roleRevokedEvent.parameters.push(
-    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
-  )
-  roleRevokedEvent.parameters.push(
-    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
-  )
-  roleRevokedEvent.parameters.push(
-    new ethereum.EventParam("sender", ethereum.Value.fromAddress(sender))
-  )
-
-  return roleRevokedEvent
 }
 
 export function createWithdrawProcessedEvent(
